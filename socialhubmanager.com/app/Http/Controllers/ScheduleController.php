7627d30459704dab548df948f00accb145a3f492
CorralesK <kimcorralesv@gmail.com>
2024-07-20 19:34:30 -0600

    Add controller, model and routes for schedules

---
+++ socialhubmanager.com/app/Http/Controllers/ScheduleController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Schedule;
+use Illuminate\Validation\Rule;
+
+class ScheduleController extends Controller
+{
+    public function index()
+    {
+        return view('schedules.index', [
+            'schedules' => Schedule::all()
+        ]);
+    }
+
+    public function create()
+    {
+        return view('schedules.create');
+    }
+
+    public function store(Request $request)
+    {
+
+        $attributes = array_merge($this->validateSchedule(), [
+            'user_id' => $request->user()->id
+        ]);
+
+        Schedule::create($attributes);
+
+        return redirect('schedules.index')->with('success', 'Horario creado exitosamente.');
+    }
+
+    public function edit(Schedule $schedule)
+    {
+        return view('schedules.edit', ['$schedule' => $schedule]);
+    }
+
+    public function update(Schedule $schedule, Request $request)
+    {
+        $attributes = $this->validateSchedule($schedule);
+
+        $schedule->update($attributes);
+
+        return redirect('schedules.index')->with('success', 'Horario actualizado exitosamente.');
+    }
+
+    public function destroy(Schedule $schedule)
+    {
+        $schedule->delete();
+
+        return back()->with('success', 'Horario eliminado exitosamente.');
+    }
+
+    protected function validateSchedule(?Schedule $schedule = null): array
+    {
+        $schedule ??= new Schedule();
+
+        return request()->validate([
+            'day_of_week' => ['required', Rule::in(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])],
+            'time' 
+                => ['required', 'date_format:H:i', 
+                Rule::unique('schedules')->where(function ($query) {
+                    return $query->where('user_id', request()->user()->id)
+                                 ->where('day_of_week', request('day_of_week'));
+                })->ignore($schedule)],
+        ], [
+            'time.unique' => 'Ya existe un horario para ese día y hora.'
+        ]);
+    }
+}
--- socialhubmanager.com/routes/web.php
+++ socialhubmanager.com/routes/web.php
@@ -1,17 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ScheduleController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
+// Schedules
+Route::get('schedules', [ScheduleController::class, 'index']);
+
+Route::get('schedules/create', [ScheduleController::class, 'create']);
+Route::post('schedules', [ScheduleController::class, 'store']);
+
+Route::get('schedules/{schedule}/edit', [ScheduleController::class, 'edit']);
+Route::patch('schedules/{schedule}', [ScheduleController::class, 'update']);
+
+Route::delete('schedules/{schedule}', [ScheduleController::class, 'destroy']);
+
+
 
 Route::get('/', function () {
     return view('welcome');
