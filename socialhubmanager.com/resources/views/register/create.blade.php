dba817103e460d4dccd3c72cdbcee605651a4231
Cristopher <cristopheralvarado101@gmail.com>
2024-07-21 00:02:22 -0600

    Create view 'create.blade.php' to register users and a panel component

---
+++ socialhubmanager.com/resources/views/register/create.blade.php
@@ -0,0 +1,18 @@
+<x-layout>
+    <section class="px-6 py-8 my-10 ">
+        <main class="max-w-lg mx-auto mt-10">
+            <x-panel>
+                <h1 class="text-center font-bold text-xl">Register in Social Hub Manager</h1>
+
+                <form method="POST" action="/register" class="mt-10">
+                    @csrf
+
+                    <x-form.input name="name" />
+                    <x-form.input name="email" type="email" />
+                    <x-form.input name="password" type="password" autocomplete="new-password" />
+                    <x-form.button>Sign Up</x-form.button>
+                </form>
+            </x-panel>
+        </main>
+    </section>
+</x-layout>
