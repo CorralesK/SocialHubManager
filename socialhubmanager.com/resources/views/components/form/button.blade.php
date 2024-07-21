f8c8193c33bf46d903d7e8ade1711f1764751472
Cristopher <cristopheralvarado101@gmail.com>
2024-07-20 23:59:23 -0600

    Add new form components and modify button

--- socialhubmanager.com/resources/views/components/form/button.blade.php
+++ socialhubmanager.com/resources/views/components/form/button.blade.php
@@ -1,6 +1,8 @@
-<div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
-    <button type="submit"
-        class="px-10 font-semibold rounded-full bg-violet-600 text-white uppercase text-xs py-2 hover:bg-blue-600">
+<x-form.field>
+    <button
+        type="submit"
+        class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600"
+    >
         {{ $slot }}
     </button>
-</div>
+</x-form.field>
