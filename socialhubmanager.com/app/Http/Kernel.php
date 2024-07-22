8c58d3df2861ab32228f1b0158f86b0d75b4a3da
Cristopher <cristopheralvarado101@gmail.com>
2024-07-22 16:46:59 -0600

    Install and configure Google2FA-Laravel package for 2FA

--- socialhubmanager.com/app/Http/Kernel.php
+++ socialhubmanager.com/app/Http/Kernel.php
@@ -65,5 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'user' => \App\Http\Middleware\ValidateUserSession::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        '2fa' => \PragmaRX\Google2FALaravel\Middleware::class,
     ];
 }
--- socialhubmanager.com/config/app.php
+++ socialhubmanager.com/config/app.php
@@ -174,6 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        PragmaRX\Google2FALaravel\ServiceProvider::class,
 
     ],
 
@@ -229,6 +230,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Google2FA' => PragmaRX\Google2FALaravel\Facade::class,
 
     ],
 
