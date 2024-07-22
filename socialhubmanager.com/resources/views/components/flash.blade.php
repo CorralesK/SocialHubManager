f1352bb0d86955e5f094880d3ab013ad1d51016e
CorralesK <kimcorralesv@gmail.com>
2024-07-22 17:17:14 -0600

    Modification of the 'flash' and 'setting' components

--- socialhubmanager.com/resources/views/components/flash.blade.php
+++ socialhubmanager.com/resources/views/components/flash.blade.php
@@ -1,12 +1,12 @@
 @if (session('success'))
     <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 4000)" x-show="show"
-        class="fixed bottom-4 right-4 bg-blue-500 text-white p-4 rounded">
+        class="fixed bottom-4 right-4 bg-blue-500 text-white p-4 rounded-2xl">
         {{ session('success') }}
     </div>
 
 @elseif (session('error'))
     <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 4000)" x-show="show"
-        class="fixed bottom-4 right-4 bg-blue-500 text-red p-4 rounded">
+        class="fixed bottom-4 right-4 bg-blue-500 text-red p-4 rounded-2xl">
         {{ session('error') }}
     </div>
 @endif
--- socialhubmanager.com/resources/views/components/setting.blade.php
+++ socialhubmanager.com/resources/views/components/setting.blade.php
@@ -1,24 +1,27 @@
 @props(['heading'])
 
 <section class="px-6 py-8">
-    <main class="max-w-4px mx-auto mt-10">
-
-        <h1 class="text-lg font-bold mb-8 pb-2 border-b"> {{ $heading }} </h1>
+    <main class="max-w-4px mx-auto mt-5">
 
         <div class="flex">
-            <aside class="w-48 flex-shrink-0">
-                <h4 class="font-semibold mb-4"> Menú </h4>
-                <ul>
+            <aside class="w-48 flex-shrink-0 bg-gray-100 rounded-2xl py-2 px-2 mr-5">
+                <h4 class="text-xl font-medium text-gray-800 tracking-wider text-center mb-4"> Menu </h4>
+                <ul class="space-y-2">
                     <li>
-                        <a href="#" class="{{ request()->is('/') ? 'text-blue-500' : '' }}"> amodificar </a>
+                        <a href="schedules" class="{{ request()->is('schedules') ? 'text-blue-500' : '' }}">
+                            Publications Schedules
+                        </a>
                     </li>
                     <li>
-                        <a href="#" class="{{ request()->is('/') ? 'text-blue-500' : '' }}"> A modificar </a>
+                        <a href="#" class="{{ request()->is('') ? 'text-blue-500' : '' }}"> A modificar </a>
                     </li>
                 </ul>
             </aside>
 
             <section class="flex-1">
+                <h1 class="text-xl font-medium text-gray-800 tracking-wider mb-8 pb-2 pt-2 border-b"> {{ $heading }}
+                </h1>
+
                 {{ $slot }}
             </section>
         </div>
