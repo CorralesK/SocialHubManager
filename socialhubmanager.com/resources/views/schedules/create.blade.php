c843525d8a9e4243de642ab07ac4c22a6c09cddb
CorralesK <kimcorralesv@gmail.com>
2024-07-22 17:15:18 -0600

    Create view 'create.blade.php' and 'create.blade.php' to register and edit schedules

---
+++ socialhubmanager.com/resources/views/schedules/create.blade.php
@@ -0,0 +1,25 @@
+<x-layout>
+    <x-setting heading="Add New Schedule">
+        <div class="overflow-x-auto">
+            <x-panel>
+                <form method="POST" action="/schedules" enctype="multipart/form-data" class="mt-2">
+                    @csrf
+
+                    <x-form.field>
+                        <x-form.label name="day of week" />
+                        <select name="day_of_week" id="day_of_week" required class="border border-gray-200 p-2 w-full rounded">
+                            @foreach (['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'] as $day)
+                                <option value="{{ $day }}" {{ old('day_of_week') == $day ? 'selected' : '' }}
+                                    class="block text-left px-3 text-sm leading-6 text-black hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white">
+                                    {{ ucfirst($day) }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </x-form.field>
+                    <x-form.input name="time" type="time"/>
+                    <x-form.button> Save </x-form.button>
+                </form>
+            </x-panel>
+        </div>
+    </x-setting>
+</x-layout>
