c2e2c0ac88aa0a6b1cd1aa10d92e95c15337bc9a
CorralesK <kimcorralesv@gmail.com>
2024-07-25 18:20:24 -0600

    Add startup vita without styles and adjustment in the “layout” component

---
+++ socialhubmanager.com/resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<x-layout>
+    <section class="px-6 py-8 my-10">
+        <main class="max-w-lg mx-auto mt-10">
+            <h1 class="text-xl font-medium text-gray-800 tracking-wider mb-8 pb-2 pt-2">
+                Social Networking
+            </h1>
+            <p class="text-xs font-medium text-gray-500 tracking-wider mb-8 pb-2 pt-2">
+                Which social network would you like to connect with?
+            </p>
+            <div class="flex">
+                <section class="">
+                    <a href="auth/twitter" class="">
+                        X
+                    </a>
+                </section>
+            </div>
+        </main>
+    </section>
+</x-layout>
