a773f0b81f42214f4000a2332ef057cfaf8335fb
CorralesK <kimcorralesv@gmail.com>
2024-08-11 18:26:52 -0600

    Add styling to the home page

--- socialhubmanager.com/resources/views/home.blade.php
+++ socialhubmanager.com/resources/views/home.blade.php
@@ -1,25 +1,23 @@
 <x-layout>
-    <section class="px-6 py-8 my-10">
-        <main class="max-w-lg mx-auto mt-10">
-            <h1 class="text-xl font-medium text-gray-800 tracking-wider mb-8 pb-2 pt-2">
-                Social Networking
-            </h1>
-            <p class="text-xs font-medium text-gray-500 tracking-wider mb-8 pb-2 pt-2">
+    <x-setting heading="Social Networking">
+        <div class="flex flex-col justify-center items-center min-h-[800px]">
+            <p class="text-xs text-gray-500 tracking-wider mb-8 pb-2 pt-2">
                 Which social network would you like to connect with?
             </p>
-            <div class="">
-                <section class="flex flex-col">
-                    <a href="auth/twitter" class="">
-                        X
-                    </a>
-                    <a href="/auth/linkedin" class="mt-5">
-                        Linkedin
-                    </a>
-                    <a href="/auth/mastodon" class="mt-5">
-                        Mastodon.social
-                    </a>
-                </section>
-            </div>
-        </main>
-    </section>
+            <section class="flex justify-center items-center mt-10 space-x-20">
+                <a href="auth/twitter" class="flex flex-col items-center text-center transform transition hover:scale-110">
+                    <x-social-icon socialNetwork="twitter" size="50" class="mb-2" />
+                    <p class="text-sm">Twitter</p>
+                </a>
+                <a href="/auth/linkedin" class="flex flex-col items-center text-center transform transition text-blue-700 hover:scale-110">
+                    <x-social-icon socialNetwork="linkedin" size="50" class="mb-2" />
+                    <p class="text-sm">LinkedIn</p>
+                </a>
+                <a href="/auth/mastodon" class="flex flex-col items-center text-center transform transition text-indigo-900 hover:scale-110">
+                    <x-social-icon socialNetwork="mastodon" size="50" class="mb-2" />
+                    <p class="text-sm">Mastodon</p>
+                </a>
+            </section>
+        </div>
+    </x-setting>
 </x-layout>
